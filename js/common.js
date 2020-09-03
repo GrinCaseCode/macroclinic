922f92a01d7aa8943906d66f0b564c7c35e7c3a9
nickolay.grin <grinmail84@gmail.com>
2020-09-03 11:16:44 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+$(".btn-main_map").click(function(event) {
+		event.preventDefault();
+		$(".modal-map").fadeOut(0);
+		var selectMap = $(this).attr("href");
+		$(selectMap).fadeIn(200);
+		$(".main-map").addClass("active");
 
+	});
 $("#map").on( "click", function () { 
 $(".btn-main_map").click(function(event) {
 		event.preventDefault();
@@ -7,11 +14,23 @@ $(".btn-main_map").click(function(event) {
 		var selectMap = $(this).attr("href");
 		$(selectMap).fadeIn(200);
 		$(".main-map").addClass("active");
+
+		 var el = $(this);
+    var dest = el.attr('href'); // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+        }, 500 // скорость прокрутки
+        );
+    }
+    return false;
 	});
  });
 
 
-$(document).mouseup(function (e){ 
+{
+ if ($(window).width() > 992) { 
+ $(document).mouseup(function (e){ 
 	var div = $(".modal-map"); 
 	if (!div.is(e.target) 
 		&& div.has(e.target).length === 0) { 
@@ -19,6 +38,9 @@ $(document).mouseup(function (e){
 	$(".main-map").removeClass("active");
 }
 });
+ }
+}
+
 
 });
 
